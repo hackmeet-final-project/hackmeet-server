dc8b62917942508fed8fbc746f838c38b1e2ff79
= <alvinbudihartono123@gmail.com>
2023-07-29 16:43:38 +0700

    feat: update mmr

--- routers/index.js
+++ routers/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const UserController = require('../controllers/userController')
 const ProfileController = require('../controllers/profileController')
 const { verifyToken } = require('../helpers/jwt')
-const {User} = require('../models')
+const { User } = require('../models')
 const router = express.Router()
 
 // Users router
@@ -13,14 +13,14 @@ router.post('/users/login', UserController.login)
 // Profiles router
 router.use(async (req, res, next) => {
     try {
-        let {access_token} = req.headers
-        if(!access_token) {
-            throw {name: "INVALIDTOKEN"}
+        let { access_token } = req.headers
+        if (!access_token) {
+            throw { name: "INVALIDTOKEN" }
         }
         let payload = verifyToken(access_token)
         let user = await User.findByPk(payload.id)
-        if(!user) {
-            throw {name: "INVALIDTOKEN"}
+        if (!user) {
+            throw { name: "INVALIDTOKEN" }
         }
         req.user = {
             id: payload.id
@@ -33,5 +33,6 @@ router.use(async (req, res, next) => {
 
 router.post('/profiles', ProfileController.createProfile)
 router.get('/profiles', ProfileController.getUserProfile)
+router.patch("/profiles", ProfileController.patchMmr);
 
 module.exports = router
